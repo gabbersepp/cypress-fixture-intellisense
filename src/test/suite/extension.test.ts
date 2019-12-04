23dcd1ca9451b510a853500e9f44355a0e59f5e0
gabbersepp <gabbersepp@googlemail.com>
2019-12-04 16:12:26 +0100

    add tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,8 +8,26 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test("extension should be loaded", () => {
+		const extension = vscode.extensions.getExtension("josefbiehler.cypress-fixture-intellisense");
+		
+		if (typeof extension === "undefined") {
+			assert.fail("extension is undefined");
+			return;
+		}
+
+		assert.equal(extension.isActive, true);
 	});
+
+	test.only("suggestion should be enabled", async (done) => {
+		
+		const file = await vscode.workspace.findFiles("**/file1.js");
+		const testDocument = await vscode.workspace.openTextDocument(file[0]);
+		const testEditor = await vscode.window.showTextDocument(testDocument);
+		const pos = new vscode.Position(0, 12);
+
+		vscode.commands.executeCommand("vscode.executeCompletionItemProvider", testDocument.uri, pos).then(() => {
+			done();
+		});
+	}).timeout(20000);
 });
